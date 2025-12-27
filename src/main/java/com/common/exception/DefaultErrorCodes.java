2ff4b989f7ab936348e509e4220bb572047c3c22
Kamal Kag <kkag840@gmail.com>
2025-12-27 15:59:34 +0530

    Initial commit: Add exception handler with logging and localized messages

---
+++ src/main/java/com/common/exception/DefaultErrorCodes.java
@@ -0,0 +1,30 @@
+package com.common.exception;
+
+public enum DefaultErrorCodes implements ErrorCodes {
+
+    NOT_NULL_VALIDATION("COMMON_EXCEPTION_0000"),
+    DATABASE_EXCEPTION("COMMON_EXCEPTION_0001"),
+    VALIDATION_FAILED("COMMON_EXCEPTION_0002"),;
+
+    private final String value;
+
+    DefaultErrorCodes(String value) {
+        this.value = value;
+    }
+
+    @Override
+    public String getValue() {
+        return value;
+    }
+
+    @Override
+    public String getName() {
+        return this.name();
+    }
+
+    @Override
+    public String getDefaultMessage() {
+        return null;
+    }
+
+}
---
+++ src/main/java/com/common/exception/ErrorCodes.java
@@ -0,0 +1,11 @@
+package com.common.exception;
+
+public interface ErrorCodes {
+
+	public String getValue();
+
+	public String getName();
+
+	public String getDefaultMessage();
+	
+}
